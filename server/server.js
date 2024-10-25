a2f933286101c384a1def099db22dbe7da434ee1
Felipe Santos <fe.campossantos@gmail.com>
2024-10-25 11:12:20 -0300

    feat: got code from course to watch videos

--- server/server.js
+++ server/server.js
@@ -1,24 +1,30 @@
+import cors from "cors";
+import express from "express";
+import { authMiddleware, handleLogin } from "./auth.js";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
+import { readFile } from "node:fs/promises";
+import { resolvers } from "./resolvers.js";
 
-const typeDefs = `#graphql 
-    schema {
-        query: Query
-    }
-    
-    type Query {
-        greeting: String
-    }
-`;
+const PORT = 9000;
 
-const resolvers = {
-  Query: {
-    greeting: () => "Hello, world!",
-  },
-};
+const app = express();
+app.use(cors(), express.json(), authMiddleware);
 
-const server = new ApolloServer({ typeDefs, resolvers });
+app.post("/login", handleLogin);
 
-const { url } = await startStandaloneServer(server, { listen: { port: 9000 } });
+const typeDefs = await readFile("./schema.graphql", "utf8");
 
-console.log(`Server running at ${url}`);
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+await apolloServer.start();
+
+app.use("/graphql", apolloMiddleware(apolloServer));
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
+});
